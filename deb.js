7a1fbfa5acdbea753e3fdd00b25d80256831baf8
BERTE BAKARY <bbaki9919@gmail.com>
2023-01-23 22:46:00 +0100

    retrait affichage jours

--- deb.js
+++ deb.js
@@ -6,28 +6,3 @@ const operand2 = 45;
 let result = operand + operand2;
 
 console.log("resultat de" + operand + "+" + operand2 + "=" + result);
-
-let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Dimanche";
-    break;
-  case 1:
-    day = "Lundi";
-    break;
-  case 2:
-     day = "Mardi";
-    break;
-  case 3:
-    day = "Mercredi";
-    break;
-  case 4:
-    day = "Jeudi";
-    break;
-  case 5:
-    day = "Vendredi";
-    break;
-  case 6:
-    day = "Samedi";
-}
-console.log("Aujourd'hui nous sommes " + day);
